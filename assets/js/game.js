f6b66de9d94c6a302dbee0c8bc1a8467af8d8c1c
Patricia Nelson <urbanearthymama@gmail.com>
2021-08-16 16:15:24 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -43,8 +44,21 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
   // if player choses to skip
 } else if (promptFight === "skip" || promptFight === "SKIP") {
-  window.alert(playerName + " has chosen to skip the fight!");
-} else {
+  // confirm player wants to skip
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+  //if yes (true), leave fight
+  if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney -2;
+  }
+  // if no (false), ask question again by running fight() again
+  else {
+     fight();
+  }
+}
+else {
   window.alert("You need to choose a valid option. Try again!");
 }
 
